731f16d1de20266f24da1876aaab667881778741
Haidercc10 <cantillohaider10@gmail.com>
2023-08-03 09:17:12 -0500

    Cambios

---
+++ PlasticaribeAPI/Migrations/20230803140917_AdicionCampo_FacturasInverGoalSuez.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PlasticaribeAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class AdicionCampo_FacturasInverGoalSuez : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Restar_DashboardCostos",
+                table: "Facturas_Invergoal_Inversuez",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Restar_DashboardCostos",
+                table: "Facturas_Invergoal_Inversuez");
+        }
+    }
+}
