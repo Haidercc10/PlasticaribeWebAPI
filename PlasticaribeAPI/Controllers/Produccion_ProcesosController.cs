9700ec9c0c65b4eb898c31a645c64d15e4694bd2
Diego Fernandez <diegofe21605@gmail.com>
2023-12-04 16:26:16 -0500

    Nomina Sellado

--- PlasticaribeAPI/Controllers/Produccion_ProcesosController.cs
+++ PlasticaribeAPI/Controllers/Produccion_ProcesosController.cs
@@ -237,6 +237,118 @@ namespace PlasticaribeAPI.Controllers
 #pragma warning restore CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
         }
 
+        //funcion que devolverá el primer rollo pesado
+        private IQueryable<long> PrimerRolloPesado(DateTime fecha)
+        {
+#pragma warning disable CS8602 // Dereference of a possibly null reference.
+            return from PS in _context.Set<Produccion_Procesos>()
+                   where PS.Fecha == fecha &&
+                         !PS.Hora.StartsWith("00") &&
+                         !PS.Hora.StartsWith("01") &&
+                         !PS.Hora.StartsWith("02") &&
+                         !PS.Hora.StartsWith("03") &&
+                         !PS.Hora.StartsWith("04") &&
+                         !PS.Hora.StartsWith("05") &&
+                         !PS.Hora.StartsWith("06")
+                   orderby PS.Numero_Rollo ascending
+                   select PS.Numero_Rollo;
+#pragma warning restore CS8602 // Dereference of a possibly null reference.
+        }
+
+        // Funcion que devolverá todos los rollos pesados en el día que se le pase y los ordenará de manera descendente
+        private IQueryable<long> UltimosRolloPesado(DateTime fecha)
+        {
+            return from PS2 in _context.Set<Produccion_Procesos>()
+                   where PS2.Fecha == fecha
+                   orderby PS2.Numero_Rollo descending
+                   select PS2.Numero_Rollo;
+        }
+
+        // Funcion que devolverá todos los rollos que hayan sido pesados en la fecha que se le pase y que sean solamente de las horas 7, 8 y 9
+        private IQueryable<long> RollosPesadosMadrugada(DateTime fecha)
+        {
+#pragma warning disable CS8602 // Dereference of a possibly null reference.
+            return from PS2 in _context.Set<Produccion_Procesos>()
+                   where (PS2.Hora.StartsWith("07") ||
+                         PS2.Hora.StartsWith("08") ||
+                         PS2.Hora.StartsWith("09") ||
+                         !PS2.Hora.StartsWith("0")) &&
+                         PS2.Fecha == fecha
+                   orderby PS2.Numero_Rollo ascending
+                   select PS2.Numero_Rollo;
+#pragma warning restore CS8602 // Dereference of a possibly null reference.
+        }
+
+        [HttpGet("getNominaSellado/{inicio}/{fin}/{item}/{persona}")]
+        public ActionResult GetNominaSellado(DateTime inicio, DateTime fin, long item, long persona)
+        {
+#pragma warning disable CS8602 // Dereference of a possibly null reference.
+            var nomina = from prod in _context.Set<Produccion_Procesos>()
+                         where prod.Proceso_Id == "SELLA" &&
+                               prod.Fecha >= inicio &&
+                               prod.Fecha <= fin &&
+                               (prod.Operario1_Id == persona || prod.Operario2_Id == persona || prod.Operario3_Id == persona || prod.Operario4_Id == persona) &&
+                               prod.Prod_Id == item &&
+                               prod.Numero_Rollo >= PrimerRolloPesado(inicio).FirstOrDefault() &&
+                               (RollosPesadosMadrugada(inicio.AddDays(1)).Any() ? prod.Numero_Rollo < RollosPesadosMadrugada(fin.AddDays(1)).FirstOrDefault() : prod.Numero_Rollo <= UltimosRolloPesado(fin).FirstOrDefault())
+                         select new
+                         {
+                             prod.Operario1_Id,
+                             prod.Operario2_Id,
+                             prod.Operario3_Id,
+                             prod.Operario4_Id,
+                             Operario1 = prod.Operario1.Usua_Nombre,
+                             Operario2 = prod.Operario2.Usua_Nombre,
+                             Operario3 = prod.Operario3.Usua_Nombre,
+                             Operario4 = prod.Operario4.Usua_Nombre,
+                             prod.OT,
+                             prod.Fecha,
+                             prod.Hora,
+                             prod.Prod_Id,
+                             prod.Producto.Prod_Nombre,
+                             prod.Numero_Rollo,
+                             Cantidad = (prod.Cantidad / prod.Pesado_Entre),
+                             CantidadTotal = prod.Cantidad,
+                             prod.Presentacion,
+                             prod.Maquina,
+                             prod.Peso_Neto,
+                             prod.Turno_Id,
+                             prod.Proceso_Id,
+                             prod.Proceso.Proceso_Nombre,
+                             prod.Precio,
+                             prod.Pesado_Entre,
+                             Total = (prod.Cantidad * prod.Precio) / prod.Pesado_Entre,
+                         };
+
+            var result = new List<object>();
+            foreach (var res in nomina)
+            {
+                string data = $"'Fecha': '{res.Fecha} {res.Hora}'," +
+                    $"'Ot': '{res.OT}'," +
+                    $"'Bulto': '{res.Numero_Rollo}'," +
+                    $"'Referencia': '{res.Prod_Id}'," +
+                    $"'Nombre_Referencia': '{res.Prod_Nombre.Replace("'", "`").Replace('"', '`')}'," +
+                    $"'Cantidad_Total': '{res.CantidadTotal}'," +
+                    $"'Cantidad': '{res.Cantidad}'," +
+                    $"'Presentacion': '{res.Presentacion}'," +
+                    $"'Maquina': '{res.Maquina}'," +
+                    $"'Peso': '{res.Peso_Neto}'," +
+                    $"'Turno': '{res.Turno_Id}'," +
+                    $"'Proceso': '{res.Proceso_Nombre}'," +
+                    $"'Precio': '{res.Precio}'," +
+                    $"'Valor_Total': '{res.Total}'," +
+                    $"'Pesado_Entre': '{res.Pesado_Entre}' ";
+
+                if (res.Operario1_Id == persona) result.Add($"'Cedula': '{res.Operario1_Id}', 'Operario': '{res.Operario1}', {data}");
+                if (res.Operario2_Id == persona) result.Add($"'Cedula': '{res.Operario2_Id}', 'Operario': '{res.Operario2}', {data}");
+                if (res.Operario3_Id == persona) result.Add($"'Cedula': '{res.Operario3_Id}', 'Operario': '{res.Operario3}', {data}");
+                if (res.Operario4_Id == persona) result.Add($"'Cedula': '{res.Operario4_Id}', 'Operario': '{res.Operario4}', {data}");
+            }
+
+            return result.Count() > 0 ? Ok(result) : NotFound();
+#pragma warning restore CS8602 // Dereference of a possibly null reference.
+        }
+
         [HttpPut("putEnvioZeus{rollo}")]
         public async Task<IActionResult> PutEnvioZeus(long rollo)
         {
